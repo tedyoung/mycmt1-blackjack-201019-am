09257f96a4f44782f3745ae9c726c2ace0ef5092
Ted M. Young <tedyoung@gmail.com>
2020-10-21 21:32:35 -0700

    Implemented balance and related addMoney functionality, with a refactoring.

--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -24,4 +24,33 @@ public class WalletTest {
         .isFalse();
   }
 
+  @Test
+  public void newWalletHasZeroBalance() throws Exception {
+    Wallet wallet = new Wallet();
+
+    assertThat(wallet.balance())
+        .isZero();
+  }
+
+  @Test
+  public void addMoneyOf10BalanceIs10() throws Exception {
+    Wallet wallet = new Wallet();
+
+    wallet.addMoney(10);
+
+    assertThat(wallet.balance())
+        .isEqualTo(10);
+  }
+
+  @Test
+  public void addMoneyOf9And8BalanceIs17() throws Exception {
+    Wallet wallet = new Wallet();
+
+    wallet.addMoney(9);
+    wallet.addMoney(8);
+
+    assertThat(wallet.balance())
+        .isEqualTo(9 + 8);
+  }
+
 }
